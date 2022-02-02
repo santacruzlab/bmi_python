4e175cc78b2d92ca45634f3ce68f9ecad901356c
DerekYJC <derek706586@gmail.com>
2022-02-02 12:37:43 -0600

    UDP to TCP Update

--- riglib/ripple/__init__.py
+++ riglib/ripple/__init__.py
@@ -30,7 +30,7 @@ class Spikes(DataSourceSystem):
     def __init__(self, channels):
         #self.conn = xp
 #        self.conn = cerelink.Connection()        
-        xp._open() #HS tcp vs udp mode
+        xp._open('tcp') #HS tcp vs udp mode
         self.channels = channels
         self.chan = itertools.cycle(self.channels)
         recChans = np.array(xp.list_elec('nano'))
